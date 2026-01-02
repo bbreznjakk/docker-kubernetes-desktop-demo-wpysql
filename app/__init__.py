f15907dbcfc04c41bf711360e4d62a8954150d31
ERDEM ERBABA <erdemerbaba@gmail.com>
2026-01-02 16:22:55 +0300

    Set up Flask app with database initialization
    Initializes the Flask application, configures extensions, and registers blueprints. Ensures the books table exists before handling requests.

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,25 @@
-will be added soon
+from flask import Flask
+from .models import db
+from routes import main_bp
+from sqlalchemy import text
+
+def create_app(config_class='config.config'):
+    """Initializes the Flask application, configures extensions and registers blueprints."""
+    app = Flask(__main__)
+    app.config.from_object(config_class)
+    db.init_app(app)
+    app.register_blueprint(main_bp)
+
+  @app.before_request
+  def create_tables():
+      """Ensures the books table exists before handling any requests.psql -U postgres"""
+      with app.app_context():
+          try:
+              # Check if tables are present by attempting a simple query
+              db.session.execute(text("SELECT 1 FROM books LIMIT 1"))
+          except Except:
+              # If query fails, create all tables defined in models.py
+              print("Database structure not found. Creating taables now...")
+              db.create_all()
+              db.session.commit()
+  return app
